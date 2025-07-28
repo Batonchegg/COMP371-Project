b0db3988dc841c26b2ad0b6122fb722ad0c696c2
parsa <parsarashidi1@gmail.com>
2025-07-28 14:41:22 -0400

    New title

--- project_V2.cpp
+++ project_V2.cpp
@@ -465,7 +465,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(1200, 700, "Parallelepiped Scene with Textured Spheres", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(1200, 700, "Assignment 1", nullptr, nullptr);
     if (!window)
     {
         std::cerr << "Failed to create GLFW window\n";
